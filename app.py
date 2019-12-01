dd007657aab38ad93bc35441f12709e5550bf2e9
srujanreddy09 <srujanreddy09@gmail.com>
2019-11-30 21:12:58 -0500

    First draft of task 4. Update1

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-from plotters import *
+from dash.exceptions import PreventUpdate
+from readers import *
+import plotly.graph_objs as go
+from plotly.subplots import make_subplots
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
